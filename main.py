967bb44a57af88d66a71a79ac9b26ad43ff02687
Murilo <murilojusti2006@gmail.com>
2025-06-29 18:25:10 -0300

    feat: adding audio download function

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import openai
 import pytubefix
+from pytubefix import YouTube
 import ffmpeg
+
+def download_audio(url):
+    try:
+        yt = YouTube(url, on_complete_callback=True)
+        stream = yt.streams.filter(only_audio=True).get_by_itag(251)
+        stream.download()
+    except:
+        print("Error downloading video")
