03404b91d63afe3f08d91f652bf0e03431bfd027
Murilo <murilojusti2006@gmail.com>
2025-06-29 23:43:44 -0300

    feat: adding audio transcription prototype

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-import openai
+from openai import OpenAI
 import pytubefix
+import os
 from pytubefix import YouTube
-import ffmpeg
+
 
 def download_audio(url):
     try:
         yt = YouTube(url, on_complete_callback=True)
         stream = yt.streams.filter(only_audio=True).get_by_itag(251)
+        print(yt.title)
         stream.download()
+
     except:
         print("Error downloading video")
+
+
+def transcript_audio():
+    client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+    transcription = client.audio.transcriptions.create(
+        model= "gpt-4o-mini-transcribe", 
+        file= open(audio_name, "rb")
+    )
+
+    print(transcription.text)
+
+download_audio(input("Link: "))
+transcript_audio()
