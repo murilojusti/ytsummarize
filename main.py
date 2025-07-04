f2aca038dc3ff4f960e9be0f62dc03f529d850f9
Murilo <murilojusti2006@gmail.com>
2025-07-03 21:41:48 -0300

    feat: audio convertion to mp3

--- main.py
+++ main.py
@@ -1,28 +1,42 @@
 from openai import OpenAI
-import pytubefix
+import static_ffmpeg
+import ffmpeg
 import os
 from pytubefix import YouTube
 
+static_ffmpeg.add_paths()
 
 def download_audio(url):
     try:
         yt = YouTube(url, on_complete_callback=True)
         stream = yt.streams.filter(only_audio=True).get_by_itag(251)
         print(yt.title)
-        stream.download()
-
+        stream.download(filename="audio")
     except:
-        print("Error downloading video")
+        print("Error downloading audio")
+
+def convert_audio():
+
+    (
+        ffmpeg
+        .input('audio')
+        .filter('atempo', 2.5)
+        .output('audio.mp3')
+        .run()
+    )
+
+    print("Error converting audio")
 
 
 def transcript_audio():
     client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
     transcription = client.audio.transcriptions.create(
         model= "gpt-4o-mini-transcribe", 
-        file= open(audio_name, "rb")
+        file= open("audio.mp3", "rb")
     )
 
     print(transcription.text)
 
 download_audio(input("Link: "))
+convert_audio()
 transcript_audio()
